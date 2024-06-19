ceff5c5c31a9e56491da5cfd9e5ea2de86860f03
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2024-06-19 13:47:47 -0400

    feat: options

--- src/app/api/actions/memo/route.ts
+++ src/app/api/actions/memo/route.ts
@@ -31,6 +31,8 @@ export const GET = async (req: Request) => {
   });
 };
 
+export const OPTIONS = GET;
+
 export const POST = async (req: Request) => {
   try {
     const body: ActionPostRequest = await req.json();
