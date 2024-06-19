a371ba04ac35308afa932b7a91b58142d52b93e8
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2024-06-19 13:39:44 -0400

    feat: vary

--- src/lib/actions/constants.ts
+++ src/lib/actions/constants.ts
@@ -21,6 +21,7 @@ export const BLINKS_QUERY_PARAM = "action";
  */
 export const ACTIONS_CORS_HEADERS: HeadersInit = {
   "Access-Control-Allow-Origin": "*",
+  Vary: "Origin",
   "Access-Control-Allow-Methods": "GET,POST,PUT,OPTIONS",
   "Access-Control-Allow-Headers":
     "Content-Type, Authorization, Content-Encoding, Accept-Encoding",
