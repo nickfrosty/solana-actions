11cbda8770d6809c86deebef581a4cb3c35e92f3
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2024-06-19 13:32:22 -0400

    feat: origin

--- src/lib/actions/constants.ts
+++ src/lib/actions/constants.ts
@@ -20,7 +20,7 @@ export const BLINKS_QUERY_PARAM = "action";
  * Standard headers
  */
 export const ACTIONS_CORS_HEADERS: HeadersInit = {
-  "Access-Control-Allow-Origin": "*",
+  // "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET,POST,PUT,OPTIONS",
   "Access-Control-Allow-Headers":
     "Content-Type, Authorization, Content-Encoding, Accept-Encoding",
