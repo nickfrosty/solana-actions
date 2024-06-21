f74ec6e4b32dec1f5aed6e014fad90273a77bfe5
nickfrosty <75431177+nickfrosty@users.noreply.github.com>
2024-06-21 11:35:25 -0400

    feat: alert and qr

--- src/app/memo/page.tsx
+++ src/app/memo/page.tsx
@@ -6,6 +6,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { siteConfig } from "@/config/site";
 import { useEffect, useState } from "react";
+import { DevnetAlert } from "@/components/devnet-alert";
 
 export default function Pages() {
   const apiPath = "/api/actions/memo";
@@ -26,6 +27,8 @@ export default function Pages() {
         "container space-y-12 bg-slate-50 py-8 dark:bg-transparent md:py-12 lg:py-24"
       }
     >
+      <DevnetAlert />
+
       <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-6 text-center">
         <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
           Simple Memo
@@ -36,13 +39,13 @@ export default function Pages() {
         </p>
       </div>
 
-      <Card className="group-hover:border-primary size-[400px] rounded overflow-hidden text-center flex items-center justify-center w-min mx-auto">
+      <Card className="group-hover:border-primary max-w-[80vw] md:max-w-[400px] aspect-square rounded overflow-clip text-center flex items-center justify-center mx-auto">
         <SolanaQRCode
           url={apiPath}
           color="white"
           background="black"
           size={400}
-          className="rounded-lg overflow-clip min-w-[400px]"
+          className="rounded-lg aspect-square [&>svg]:scale-75 md:[&>svg]:scale-100"
         />
       </Card>
 
